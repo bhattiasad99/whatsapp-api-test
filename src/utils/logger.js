769bf7253ec21e1ae0d1db4f67a9ade807306628
Asad Bhatti <bhatti.asad99@gmail.com>
2023-04-17 20:47:32 +0500

    .

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,20 +1,22 @@
-import { Log } from '../models/index.js';
-import ipHelper from './helpers/ip-helper.js';
+import { ENABLE_MONGODB_CONNECTION } from "../config/index.js";
+import { Log } from "../models/index.js";
+import ipHelper from "./helpers/ip-helper.js";
 
 export default async (code, userId, errorMessage, level, req) => {
-  let ip = 'no-ip';
-  if(req !== '') ip = ipHelper(req);
-  let log = new Log({
-    resultCode: code,
-    level: level,
-    errorMessage: errorMessage,
-    ip: ip
-  });
-
-  if (userId !== '' && userId) log.userId = userId;
-
-  await log.save()
-    .catch(err => {
-      console.log('Logging is failed: ' + err);
+  if (ENABLE_MONGODB_CONNECTION) {
+    let ip = "no-ip";
+    if (req !== "") ip = ipHelper(req);
+    let log = new Log({
+      resultCode: code,
+      level: level,
+      errorMessage: errorMessage,
+      ip: ip,
     });
-}
+
+    if (userId !== "" && userId) log.userId = userId;
+
+    await log.save().catch((err) => {
+      console.log("Logging is failed: " + err);
+    });
+  }
+};
